5aa2f9eb17abe27b3640e071143755857203fdfd
Fiziev Vagif <vagasuper560469@gmail.com>
2022-11-13 13:56:18 +0600

    1 level refactoring.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Modal from "./Components/Modal/Modal";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const modalIsOpen = () => setShowModal(true);
+  const modalIsClose = () => setShowModal(false);
+
   return (
     <>
-      <Modal/>
+      <Modal
+        show={showModal}
+        title='Some kinda modal title'
+        onClose={modalIsClose}
+      >
+        <div className="modal-body">
+          This is a modal content
+        </div>
+      </Modal>
 
-      <button className="btn btn-primary">Show modal</button>
-      <button className="btn btn-danger ms-4">Show alert</button>
+      <button
+        className="btn btn-primary"
+        onClick={modalIsOpen}
+      >
+        Show modal
+      </button>
+
+      <button
+        className="btn btn-danger ms-4"
+      >
+        Show alert
+      </button>
     </>
   );
 }
--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import Backdrop from "../Backdrop/Backdrop";
 
-const Modal = () => {
+interface Props extends React.PropsWithChildren{
+  show: boolean;
+  title: string;
+  onClose: React.MouseEventHandler;
+}
+
+const Modal: React.FC<Props> = ({show, title, onClose, children}) => {
   return (
     <>
-      <Backdrop/>
-      <div className="modal show" style={{display: 'block'}}>
-        <div className="modal-dialog">
+      <Backdrop show={show}/>
+      <div
+        className="modal show"
+        style={{display: show ? 'block' : 'none'}}
+        onClick={onClose}
+      >
+        <div className="modal-dialog" onClick={e => e.stopPropagation()}>
           <div className="modal-content">
             <div className="modal-header">
-              <h1 className="modal-title fs-5">Modal title</h1>
+              <h1 className="modal-title fs-5">{title}</h1>
+              <button type="button" className="btn-close" onClick={onClose}></button>
             </div>
-            Modal content
+            {children}
           </div>
         </div>
       </div>
