6c54b07981ee527ddef6eac6bd2b7937c04a1ec2
Fiziev Vagif <vagasuper560469@gmail.com>
2022-11-15 08:22:15 +0600

    Added Alert components.
    Wrote the main logic.

---
+++ src/Components/Alert/Alert.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface Props extends React.PropsWithChildren{
+  type: string;
+  show: boolean;
+  onDismiss?: React.MouseEventHandler;
+}
+
+const Alert: React.FC<Props> = ({type, show, onDismiss, children}) => {
+  let className = "alert alert-dismissible alert-";
+
+  let btnStyle = {display: 'block'};
+
+  if (onDismiss === undefined){
+    btnStyle = {display: 'none'};
+  }
+
+  return (
+    <>
+      <div
+        className={className + type}
+        style={{display: show ? 'block' : 'none'}}
+      >
+        {children}
+        <button type="button" className="btn-close" style={btnStyle} onClick={onDismiss}></button>
+      </div>
+    </>
+  );
+};
+
+export default Alert;
---
+++ src/Containers/App.tsx
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import Modal from "../Components/Modal/Modal";
+import Alert from "../Components/Alert/Alert";
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+
+  const modalIsOpen = () => setShowModal(true);
+  const modalIsClose = () => setShowModal(false);
+
+  const alertIsClose = () => setShowAlert(false);
+  const alertIsShow = () => setShowAlert(true);
+
+  return (
+    <>
+      <button
+        className="btn btn-primary"
+        onClick={modalIsOpen}
+      >Show modal</button>
+
+      <button
+        className="btn btn-danger ms-4"
+        onClick={alertIsShow}
+      >Show alert</button>
+
+      <Modal
+        show={showModal}
+        title='Some kinda modal title'
+        onClose={modalIsClose}
+      >
+        <div className="modal-body">
+          This is a modal content
+        </div>
+      </Modal>
+
+      <Alert
+        show={showAlert}
+        type="warning"
+        onDismiss={alertIsClose}
+      >This is a warning type alert</Alert>
+      <Alert
+        show={showAlert}
+        type="success"
+      >This is a success type alert</Alert>
+    </>
+  );
+}
+
+export default App;
